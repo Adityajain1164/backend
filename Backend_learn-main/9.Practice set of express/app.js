11f06b889efe4446bb18bf6501f7d1cd943e986d
Adityajain1164 <2023uce1164@mnit.ac.in>
2026-05-21 14:03:20 +0530

    completing practice set for express js

---
+++ Backend_learn-main/9.Practice set of express/app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.url);
+  next();
+});
+app.use((req, res, next) => {
+  console.log(req.method);
+  next();
+});
+
+// app.use((req, res, next) => {
+//   console.log("i am inside the third sending middle ware");
+
+//   res.send(`<h>response by dummy third middle ware</h>`);
+// });
+
+app.get("/", (req, res, next) => {
+  console.log("inside the home page");
+  res.send(`<a href="/contact-us">Contact Us</a>`);
+});
+
+app.get("/contact-us", (req, res, next) => {
+  console.log("inside the contact us page");
+  res.send(`
+  <h2>Contact Form</h2>
+
+  <form action="/contact-us" method="POST">
+    
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" required />
+    
+    <br><br>
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required />
+    
+    <br><br>
+
+    <label for="number">Number:</label>
+    <input type="tel" id="number" name="number" required />
+    
+    <br><br>
+
+    <button type="submit">Submit</button>
+
+  </form>
+  `);
+
+  app.post("/contact-us", (req, res, next) => {
+    console.log("inside the post request");
+    res.send(`
+    <h1>Thank You!</h1>
+    <p>Your form has been submitted successfully.</p>
+    <p>We will contact you soon.</p>
+
+    <a href="/">Go Back</a>
+    `);
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`server is runinng at http://localhost:3000`);
+});
