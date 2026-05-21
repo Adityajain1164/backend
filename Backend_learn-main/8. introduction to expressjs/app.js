c3cdd76b6514f708b4cfbfef551256c7bf2cf583
Adityajain1164 <2023uce1164@mnit.ac.in>
2026-05-21 09:29:27 +0530

    learning middle ware

--- Backend_learn-main/8. introduction to expressjs/app.js
+++ Backend_learn-main/8. introduction to expressjs/app.js
@@ -1,5 +1,7 @@
-// core modules
-const http = require("http");
+// // using express we can make server without the use of http module
+// // core modules
+// const http = require("http");
+
 const fs = require("fs");
 
 // external module
@@ -10,9 +12,38 @@ const userRequestHandler = require("./user");
 
 const app = express();
 
-const server = http.createServer(app);
+//* no need to create server and then pass app into it
+//* server will be directly created by app
+// const server = http.createServer(app);
+
+// app.use("/", (req, res, next) => {
+//   //* when ever we use "use" then the path is wildcard so any one having "/" will be matched by this
+//   console.log("inside the first middle ware", req.url, req.method);
+//   // res.send(`<h1> welcome to home page</h1>`);
+//   //* after res.send you can not call next
+//   next();
+// });
+
+app.post("/", (req, res, next) => {
+  //* get or post are very specific about the path only occour when same path
+  console.log("inside the first middle ware ", req.url, req.method);
+  next();
+});
+
+app.use("/submit-detail", (req, res, next) => {
+  console.log("inside second middle ware ", req.url, req.method);
+  res.send(`<p>this response is send by the second middle ware</p>`);
+  //* here we dont have to set the header and no need for res.write and also no need to do res.end();
+  //*express is a helper library of node only all the code is on github check it ones
+});
+
+app.use("/", (req, res, next) => {
+  console.log("inside 3rd middle ware", req.url, req.method);
+  res.send("welcome to the home page");
+});
 
 const PORT = 3000;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`server is started at http://localhost:3000`);
 });
+//*insted of server.listen we can directly use app.listen
